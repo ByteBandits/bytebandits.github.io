a2caee9a02c0d4a28e41b81f8faae66c0acb4c95
B Krishna Chaitanya <bkchaitan94@gmail.com>
2019-03-18 18:00:45 +0530

    Start using react hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,61 +1,55 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { Index } from "elasticlunr"
 import styles from '../styles/search.module.css'
 import Writeup from '../models/Writeup'
 
 // Search component
-export default class Search extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      query: ``,
-      results: [],
-    }
-  }
+export default ({searchIndex}) => {
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+  let index
 
-  render() {
-    return (
-      <div className={styles.searchBar}>
-        <input type="text" value={this.state.query} onChange={this.search} placeholder="Search writeups.." />
-        <ul>
-          {this.state.results.map(page => {
-            try {
-              const writeup = new Writeup('/' + page.path)
-              return <Link to={"/" + page.path}>
-                <li key={page.id}>
-                  <div><b>{writeup.problemName}</b></div>
-                  <div><small>{writeup.problemType}</small></div>
-                  <div><small>{writeup.ctfName}</small></div>
-                  <div><small>{writeup.author}</small></div>
-                  <div><small>{"tags: " + page.tags.join(`,`)}</small></div>
-                </li>
-              </Link>
-            } catch(e) {
-              console.error(e)
-              return undefined
-            }
-          })}
-        </ul>
-      </div>
+  const getOrCreateIndex = () =>
+    index
+      ? index
+      : // Create an elastic lunr index and hydrate with graphql query results
+        Index.load(searchIndex)
+
+  const search = evt => {
+    const query = evt.target.value
+    index = getOrCreateIndex()
+    setQuery(query)
+    setResults(
+      // Query the index with search string to get an [] of IDs
+      index.search(query, {})
+        // Map over each ID and return the full document
+        .map(({ ref }) => index.documentStore.getDoc(ref))
     )
   }
-  getOrCreateIndex = () =>
-    this.index
-      ? this.index
-      : // Create an elastic lunr index and hydrate with graphql query results
-        Index.load(this.props.searchIndex)
 
-  search = evt => {
-    const query = evt.target.value
-    this.index = this.getOrCreateIndex()
-    this.setState({
-      query,
-      // Query the index with search string to get an [] of IDs
-      results: this.index
-        .search(query, {})
-        // Map over each ID and return the full document
-        .map(({ ref }) => this.index.documentStore.getDoc(ref)),
-    })
-  }
+  return (
+    <div className={styles.searchBar}>
+      <input type="text" value={query} onChange={search} placeholder="Search writeups.." />
+      <ul>
+        {results.map(page => {
+          try {
+            const writeup = new Writeup('/' + page.path)
+            return <Link to={"/" + page.path}>
+              <li key={page.id}>
+                <div><b>{writeup.problemName}</b></div>
+                <div><small>{writeup.problemType}</small></div>
+                <div><small>{writeup.ctfName}</small></div>
+                <div><small>{writeup.author}</small></div>
+                <div><small>{"tags: " + page.tags.join(`,`)}</small></div>
+              </li>
+            </Link>
+          } catch(e) {
+            console.error(e)
+            return undefined
+          }
+        })}
+      </ul>
+    </div>
+  )
 }
--- src/pages/writeups.js
+++ src/pages/writeups.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
@@ -7,120 +7,122 @@ import styles from '../styles/writeups.module.css'
 import Writeup from '../models/Writeup'
 
 
-export default class Writeups extends React.Component {
-  state = {
-    selectedCTF: undefined,
-    selectedProblemType: undefined,
-    selectedAuthor: undefined,
+const useSelectedCTF = () => {
+  const [selectedCTF, setSelectedCTF] = useState()
+  useEffect(() => console.log('a different CTF was selected'), [selectedCTF])
+  return [selectedCTF, setSelectedCTF]
+}
+
+
+export default ({ data, location }) => {
+  const [selectedProblemType, setSelectedProblemType] = useState()
+  const [selectedCTF, setSelectedCTF] = useSelectedCTF()
+  const [selectedAuthor, setSelectedAuthor] = useState()
+
+  let writeups = data.allSitePage.edges
+    .filter(({node}) => (node.path.match(/\//g) || []).length === 5)
+    .map(({node}) => new Writeup(node.path))
+
+  const authors = writeups.map((w) => w.author).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
+  const problemTypes = writeups.map((w) => w.problemType).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
+  const ctfNames = writeups.map((w) => w.ctfName).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
+
+  const non_writeups = data.allSitePage.edges.filter(({node}) => (node.path.match(/\//g) || []).length !== 5)
+  if (non_writeups.length > 0) {
+    console.warn("Some non writeup pages were skipped")
+    console.warn(non_writeups)
   }
 
-  render() {
-    const { data, location } = this.props
+  if (selectedProblemType && selectedProblemType !== 'All') {
+    writeups = writeups.filter((w) => w.problemType === selectedProblemType)
+  }
 
-    let writeups = data.allSitePage.edges
-      .filter(({node}) => (node.path.match(/\//g) || []).length === 5)
-      .map(({node}) => new Writeup(node.path))
+  if (selectedCTF && selectedCTF !== 'All') {
+    writeups = writeups.filter((w) => w.ctfName === selectedCTF)
+  }
 
-    const authors = writeups.map((w) => w.author).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
-    const problemTypes = writeups.map((w) => w.problemType).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
-    const ctfNames = writeups.map((w) => w.ctfName).filter((elem, pos, arr) => arr.indexOf(elem) === pos)
+  if (selectedAuthor && selectedAuthor !== 'All') {
+    writeups = writeups.filter((w) => w.author === selectedAuthor)
+  }
 
-    const non_writeups = data.allSitePage.edges.filter(({node}) => (node.path.match(/\//g) || []).length !== 5)
-    if (non_writeups.length > 0) {
-      console.log("Some non writeup pages were skipped")
-      console.log(non_writeups)
-    }
-
-    if (this.state.selectedProblemType && this.state.selectedProblemType !== 'All') {
-      console.log(this.state.selectedProblemType)
-      writeups = writeups.filter((w) => w.problemType === this.state.selectedProblemType)
-    }
-
-    if (this.state.selectedCTF && this.state.selectedCTF !== 'All') {
-      console.log(this.state.selectedCTF)
-      writeups = writeups.filter((w) => w.ctfName === this.state.selectedCTF)
-    }
-
-    if (this.state.selectedAuthor && this.state.selectedAuthor !== 'All') {
-      console.log(this.state.selectedAuthor)
-      writeups = writeups.filter((w) => w.author === this.state.selectedAuthor)
-    }
-
-    return (
-      <Layout location={location}>
-        <SEO title="Writeups" />
-        <div style={{margin: '24px auto 64px'}}>
-          <h1>Writeups</h1>
-          <Flex flexWrap='wrap' mx={-2}>
-            <Box m={2}>
-            <div style={{fontSize: '0.66em', color: 'grey'}}>Type</div>
-              <select onChange={(e) => this.setState({selectedProblemType: e.target.value})}>
-                <option value={undefined}>All</option>
-                {
-                  problemTypes.map((a) =>
-                    <option value={a} key={a}>
-                      {a}
-                    </option>
-                  )
-                }
-              </select>
-            </Box>
-            <Box m={2}>
-              <div style={{fontSize: '0.66em', color: 'grey'}}>CTF</div>
-              <select onChange={(e) => this.setState({selectedCTF: e.target.value})}>
-                <option value={undefined}>All</option>
-                {
-                  ctfNames.map((a) =>
-                    <option value={a} key={a}>
-                      {a}
-                    </option>
-                  )
-                }
-              </select>
-            </Box>
-            <Box m={2}>
-              <div style={{fontSize: '0.66em', color: 'grey'}}>Author</div>
-              <select onChange={(e) => this.setState({selectedAuthor: e.target.value})}>
-                <option value={undefined}>All</option>
-                {
-                  authors.map((a) =>
-                    <option value={a} key={a}>
-                      {a}
-                    </option>
-                  )
-                }
-              </select>
-            </Box>
-          </Flex>
-          <Flex flexWrap='wrap' mx={-2} justifyContent='center' alignItems='center'>
-            {writeups.map((writeup, idx) =>
-              <a href={writeup.path} style={{
+  return (
+    <Layout location={location}>
+      <SEO title="Writeups" />
+      <div style={{margin: '24px auto 64px', minHeight: 640}}>
+        <h1>Writeups</h1>
+        <Flex flexWrap='wrap' mx={-2}>
+          <Box m={2}>
+          <div style={{fontSize: '0.66em', color: 'grey'}}>Type</div>
+            <select onChange={(e) => setSelectedProblemType(e.target.value)}>
+              <option value={undefined}>All</option>
+              {
+                problemTypes.map((a) =>
+                  <option value={a} key={a}>
+                    {a}
+                  </option>
+                )
+              }
+            </select>
+          </Box>
+          <Box m={2}>
+            <div style={{fontSize: '0.66em', color: 'grey'}}>CTF</div>
+            <select onChange={(e) => setSelectedCTF(e.target.value)}>
+              <option value={undefined}>All</option>
+              {
+                ctfNames.map((a) =>
+                  <option value={a} key={a}>
+                    {a}
+                  </option>
+                )
+              }
+            </select>
+          </Box>
+          <Box m={2}>
+            <div style={{fontSize: '0.66em', color: 'grey'}}>Author</div>
+            <select onChange={(e) => setSelectedAuthor(e.target.value)}>
+              <option value={undefined}>All</option>
+              {
+                authors.map((a) =>
+                  <option value={a} key={a}>
+                    {a}
+                  </option>
+                )
+              }
+            </select>
+          </Box>
+        </Flex>
+        <Flex flexWrap='wrap' mx={-2} justifyContent='center' alignItems='center'>
+          {writeups.map((writeup, idx) =>
+            <a
+              href={writeup.path}
+              style={{
                 color: 'inherit',
                 textDecoration: 'none',
-              }}>
-                <Card
-                  className={styles.card}
-                  style={{
-                    background: writeup.getBG(),
-                  }}
-                  p={3}
-                  m={3}
-                  bg='#ffffff'
-                  borderRadius={4}
-                  boxShadow='0 0 4px rgba(0, 0, 0, 0.16)'
-                >
-                  <p>{writeup.problemName}</p>
-                  <p>{writeup.problemType}</p>
-                  <p>{writeup.ctfName}</p>
-                  <p>{writeup.author}</p>
-                </Card>
-              </a>
-            )}
-          </Flex>
-        </div>
-      </Layout>
-    )
-  }
+              }}
+              key={idx}
+            >
+              <Card
+                className={styles.card}
+                style={{
+                  background: writeup.getBG(),
+                }}
+                p={3}
+                m={3}
+                bg='#ffffff'
+                borderRadius={4}
+                boxShadow='0 0 4px rgba(0, 0, 0, 0.16)'
+              >
+                <p>{writeup.problemName}</p>
+                <p>{writeup.problemType}</p>
+                <p>{writeup.ctfName}</p>
+                <p>{writeup.author}</p>
+              </Card>
+            </a>
+          )}
+        </Flex>
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
