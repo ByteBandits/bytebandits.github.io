88793f7abdd3c0ec6f1b641b991a3e6cbf5230a8
B Krishna Chaitanya <bkchaitan94@gmail.com>
2019-03-24 12:27:14 +0530

    Responsive header / nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import { StaticQuery, Link, graphql } from "gatsby"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
-import React from "react"
-import Headroom from 'react-headroom'
+import { StaticQuery, Link, graphql } from "gatsby"
+import styled from 'styled-components'
 import { Flex, Box } from 'rebass'
+import { MdDehaze, MdClose } from 'react-icons/md'
+import Headroom from 'react-headroom'
 import LinkAnimated from './LinkAnimated'
 import Search from "./Search"
 
@@ -13,71 +15,134 @@ const linkStyle = {
   cursor: 'pointer',
 }
 
-const Header = ({ siteTitle, location }) => (
-  <Headroom
+const ResponsiveSmall = styled.div`
+  display: none;
+  text-align: center;
+  width: 100%;
+  @media (max-width: ${props => props.size}px) {
+    display: block;
+  }
+`
+
+const ResponsiveLarge = styled.div`
+  display: block;
+  text-align: center;
+  @media (max-width: ${props => props.size}px) {
+    display: none;
+  }
+`
+
+const menu = [
+  {
+    "text": "Home",
+    "path": "/",
+  },
+  {
+    "text": "Team",
+    "path": "/team",
+  },
+  {
+    "text": "Writeups",
+    "path": "/writeups",
+  },
+]
+
+const logo = <Link
+    to="/"
+  >
+    <img height="96" alt="ByteBandits Logo" src="https://avatars3.githubusercontent.com/u/14941705?s=200&v=4" style={{ display: 'inline' }}/>
+  </Link>
+
+const searchBar = <StaticQuery
+  query={graphql`
+    query SearchIndexQuery {
+      siteSearchIndex {
+        index
+      }
+    }
+  `}
+  render={data => (
+    <Search searchIndex={data.siteSearchIndex.index} />
+  )}
+/>
+
+const Header = ({ siteTitle, location }) => {
+  const [open, setOpen] = useState(false)
+
+  return <Headroom
     style={{
       background: `#000000`,
       marginBottom: '1.45rem',
       height: 107,
+      zIndex: 100,
+      borderBottom: '2px solid green',
     }}
   >
-    <Flex
-      alignItems='center'
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-      }}
-    >
-      <Link
-        to="/"
+
+    <ResponsiveLarge size={500}>
+      <Flex
+        alignItems='center'
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+        }}
       >
-        <img height="96" alt="ByteBandits Logo" src="https://avatars3.githubusercontent.com/u/14941705?s=200&v=4" style={{ display: 'inline' }}/>
-      </Link>
-      <Box mx='auto' />
-      <Box ml={[4, 3]} color="background" fontSize={[2, 3]}>
-        <LinkAnimated
-          to="/"
-          style={linkStyle}
-          selected={location.pathname === '/'}
+        { logo }
+        <Box mx='auto' />
+        {menu.map((e) =>
+          <Box ml={[4, 3]} color="backgroundLight" bg="background" fontSize={[2, 3]}>
+            <LinkAnimated
+              to={e.path}
+              style={linkStyle}
+              selected={location.pathname === e.path}
+            >
+              {e.text}
+            </LinkAnimated>
+          </Box>
+        )}
+        <Box ml={[4, 3]} color="backgroundLight" bg="background" fontSize={[2, 3]}>
+          <ResponsiveLarge size={640}>
+            { searchBar }
+          </ResponsiveLarge>
+        </Box>
+      </Flex>
+    </ResponsiveLarge>
+
+    <ResponsiveSmall size={500}>
+      <Flex
+        alignItems='center'
+        flexWrap='wrap'
+      >
+        <Flex
+          alignItems='center'
+          width={1}
+          px={3}
         >
-          Home
-        </LinkAnimated>
-      </Box>
-      <Box ml={[4, 3]} color="background" fontSize={[2, 3]}>
-        <LinkAnimated
-          to="/team"
-          style={linkStyle}
-          selected={location.pathname === '/team'}
-        >
-          Team
-        </LinkAnimated>
-      </Box>
-      <Box ml={[4, 3]} color="background" fontSize={[2, 3]}>
-        <LinkAnimated
-          to="/writeups"
-          style={linkStyle}
-          selected={location.pathname === '/writeups'}
-        >
-          Writeups
-        </LinkAnimated>
-      </Box>
-      <Box ml={[4, 3]} color="background" fontSize={[2, 3]}>
-        <StaticQuery
-          query={graphql`
-            query SearchIndexQuery {
-              siteSearchIndex {
-                index
-              }
-            }
-          `}
-          render={data => (
-            <Search searchIndex={data.siteSearchIndex.index} />
-          )}
-        />
-      </Box>
-    </Flex>
+          { logo }
+          <Box mx='auto'/>
+          { open ?
+            <MdClose size={30} onClick={() => setOpen(false)} color="white"/> :
+            <MdDehaze size={30} onClick={() => setOpen(true)} color="white"/>
+          }
+        </Flex>
+        { open && menu.map((e) =>
+          <Box width={1} p={4} color="backgroundLight" bg="background" fontSize={[2, 3]}>
+            <LinkAnimated
+              to={e.path}
+              style={linkStyle}
+              selected={location.pathname === e.path}
+            >
+              {e.text}
+            </LinkAnimated>
+          </Box>
+        )}
+        { open && <Box width={1} p={4} color="backgroundLight" bg="background" fontSize={[2, 3]} style={{borderBottom: '2px solid green'}}>
+          { searchBar }
+        </Box> }
+      </Flex>
+    </ResponsiveSmall>
   </Headroom>
-)
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
